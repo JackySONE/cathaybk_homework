784a018216415abc912d981ad75f0558dd9f3b43
JackySONE <jackysnsd@gmail.com>
2018-11-28 21:50:22 +0800

    [feat] 新增仿照NavigationBar，實現上下滑動效果

--- cathaybkPractice/ViewControllers/AnimalListViewController.m
+++ cathaybkPractice/ViewControllers/AnimalListViewController.m
@@ -14,11 +14,18 @@
 
 #import "CommonImport.h"
 
+#define MaxHeaderHeight 200
+#define MinHeaderHeight 44
+
 @interface AnimalListViewController () <UITableViewDelegate, UITableViewDataSource>
 
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView             *headerView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerHeightConstraint;
+@property (weak, nonatomic) IBOutlet UITableView        *tableView;
 
-@property (nonatomic, strong) AnimalListViewModel *viewModel;
+@property (nonatomic, strong) AnimalListViewModel       *viewModel;
+
+@property (nonatomic, assign) CGFloat                   previousScrollOffset;
 
 @end
 
@@ -29,6 +36,13 @@
     [super viewDidLoad];
 
     [self getListData];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // Header
+    self.headerHeightConstraint.constant = MaxHeaderHeight;
 }
 
 #pragma mark - Custom Accessories
@@ -55,6 +69,51 @@
 - (void)getListData
 {
     [self.viewModel fetchItems];
+}
+
+- (BOOL)canAnimateHeader:(UIScrollView *)scrollView
+{
+    // Calculate the size of the scrollView when header is collapsed
+    CGFloat scrollViewMaxHeight = scrollView.frame.size.height + self.headerHeightConstraint.constant - MinHeaderHeight;
+
+    // Make sure that when header is collapsed, there is still room to scroll
+    return scrollView.contentSize.height > scrollViewMaxHeight;
+}
+
+- (void)setScrollPosition:(CGFloat)position
+{
+    self.tableView.contentOffset = CGPointMake(self.tableView.contentOffset.x, position);
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat scrollDiff = scrollView.contentOffset.y - self.previousScrollOffset;
+
+    CGFloat absoluteTop     = 0;
+    CGFloat absoluteBottom  = scrollView.contentSize.height - scrollView.frame.size.height;
+
+    BOOL isScrollingDwon    = scrollDiff > 0 && scrollView.contentOffset.y > absoluteTop;
+    BOOL isScrollingUp      = scrollDiff < 0 && scrollView.contentOffset.y < absoluteBottom;
+
+    if ([self canAnimateHeader:scrollView]) {
+
+        // Calculate new header height
+        CGFloat newHeight = self.headerHeightConstraint.constant;
+        if (isScrollingDwon) {
+            newHeight = MAX(MinHeaderHeight, self.headerHeightConstraint.constant - fabs(scrollDiff));
+        } else if (isScrollingUp){
+            newHeight = MIN(MaxHeaderHeight, self.headerHeightConstraint.constant + fabs(scrollDiff));
+        }
+
+        // Header needs to animate
+        if (newHeight != self.headerHeightConstraint.constant) {
+            self.headerHeightConstraint.constant = newHeight;
+            [self setScrollPosition:self.previousScrollOffset];
+        }
+
+        self.previousScrollOffset = scrollView.contentOffset.y;
+    }
 }
 
 #pragma mark - UITableViewDelegate & UITableViewDataSource
